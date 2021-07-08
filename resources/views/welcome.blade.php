2ebc8cf3790728ea6c689884e50510d8051c5e38
sofanjitok <sofanjitok@gmail.com>
2021-07-08 23:03:23 +0700

    tugas fp

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title> </title>
+    <style>
+      body {
+        background-color: #e8e8e8;
+      }
+      .kartu {
+        width: 800px;
+        margin: 0 auto;
+        margin-top: 70px;
+            box-shadow: 0 0.25rem 0.75rem rgba(0,0,0,.03);
+    transition: all .3s;
+      }
+      .foto {
+        padding: 20px;
+      }
+      tbody {
+    font-size: 20px;
+    font-weight: 300;
+}
+.biodata {
+    margin-top: 30px;
+}
+    </style>
+  </head>
+  <body>
+
+        <!-- Isi Biodata Paling Keren di HTML disini -->
+
+      </div>
+      <div class="col-md-8 kertas-biodata">
+        <div class="biodata">
+        <table width="100%" border="0">
+    <tbody><tr>
+        <td valign="top">
+        <table border="0" width="100%" style="padding-left: 2px; padding-right: 13px;">
+          <tbody>
+            <tr>
+              <td width="25%" valign="top" class="textt">Nama</td>
+                <td width="2%">:</td>
+                <td style="color: rgb(118, 157, 29); font-weight:bold">sofanji</td>
+            </tr>
+          <tr>
+              <td class="textt">Jenis Kelamin</td>
+                <td>:</td>
+                <td>Laki-Laki</td>
+            </tr>
+          <tr>
+              <td class="textt">Tempat Lahir</td>
+                <td>:</td>
+                <td>Tegal timur</td>
+            </tr>
+          <tr>
+              <td class="textt">Nim</td>
+                <td>:</td>
+                <td>18090098</td>
+            </tr>
+          <tr>
+          <tr>
+              <td valign="top" class="textt">Prodi</td>
+                <td valign="top">:</td>
+                <td>Teknik Informatika</td>
+            </tr>
+
+        </tbody></table>
+        </td>
+    </tr>
+    </tbody></table>
+  </div>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
